2f3fd8c20f799da1770ec6eb6aa8601ffe1d99d3
Youngjae <2015fromjeki@gmail.com>
2025-01-29 22:33:55 +1100

    Fix the minor issue

--- frontEnd/src/components/Button.jsx
+++ frontEnd/src/components/Button.jsx
@@ -16,11 +16,7 @@ const Button = ({
       type="button"
       className={`w-[60px] h-[40px] bg-[${style}] rounded-xl font-bold tracking-wider cursor-pointer shadow-2xl border-none shadow-red-800 font-serif hover:opacity-100  ${
         selectedCategory === text ? "opacity-100" : "opacity-50 "
-      } ${
-        quotes?.length > 0 && quotes[currentIndex]?.category == text
-          ? "opacity-100"
-          : "opacity-50 "
-      }`}
+      } `}
       onClick={() => {
         handleCategoryChange(text);
       }}
