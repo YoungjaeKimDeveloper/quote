07dee0be8de6c74ea46d284af27830ebf9d0e312
Youngjae <2015fromjeki@gmail.com>
2025-01-29 19:34:28 +1100

    Tailwind css config has been changed

--- frontEnd/tailwind.config.js
+++ frontEnd/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: ["./frontEnd/index.html", "./frontEnd/src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [],
 };
