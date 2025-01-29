8e2d904ef323020107d91bdcff11910ffad2688a
Youngjae <2015fromjeki@gmail.com>
2025-01-29 19:15:56 +1100

    Change the config

--- frontEnd/lib/axiosInstance.js
+++ frontEnd/lib/axiosInstance.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5005/quote", // 기본 URL 설정
+  withCredentials: true,
 });
 
 export default axiosInstance;
