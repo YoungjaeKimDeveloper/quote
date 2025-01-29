12b39409ced3eaf19c90f06fc039962c97c8a224
Youngjae <2015fromjeki@gmail.com>
2025-01-29 20:56:46 +1100

    Fit the error

--- frontEnd/tailwind.config.js
+++ frontEnd/tailwind.config.js
@@ -6,6 +6,10 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    { pattern: /opacity-(0|100)/ }, // opacity-0, opacity-100 유지
+    { pattern: /bg-gradient-.*/ }, // gradient 관련 클래스 유지
+  ],
   theme: {
     extend: {},
   },
