65e703dac9914848979bf63bc0abab449558af8d
Youngjae <2015fromjeki@gmail.com>
2025-01-29 19:23:42 +1100

    fixed th bug

--- backEnd/server.js
+++ backEnd/server.js
@@ -7,18 +7,17 @@ import path from "path";
 
 // 라우터
 import quoteRoutes from "./routes/quotes.routes.js";
+dotenv.config();
 const app = express();
 // 미들웨어 설정해주기
 
 app.use(express.json({ limit: "6 mb" }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://quote-qj53.onrender.com/"],
+    origin: ["http://localhost:5173", "https://quote-qj53.onrender.com"],
     credentials: true,
   })
 );
-
-dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
