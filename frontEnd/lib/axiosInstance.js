056b6920fb2ca3ddff75545c8e5555b3d2aa5bc6
Youngjae <2015fromjeki@gmail.com>
2025-01-29 19:47:14 +1100

    Fixed the config

--- frontEnd/lib/axiosInstance.js
+++ frontEnd/lib/axiosInstance.js
@@ -1,7 +1,13 @@
 import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const axiosInstance = axios.create({
-  baseURL: "https://quote-qj53.onrender.com/quote",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://quote-qj53.onrender.com/quote"
+      : "http://localhost:5005/quote",
   withCredentials: true,
 });
 
