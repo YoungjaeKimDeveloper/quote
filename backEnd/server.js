88cf244f51ab58b5f3cdc8efbac08edfa801c885
Youngjae <2015fromjeki@gmail.com>
2025-01-30 16:24:47 +1100

    Set the helmet setting

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import connectDB from "./lib/connectDB.js";
+import helmet from "helmet"; // Helmet 미들웨어 추가
 // 추가됨
 import path from "path";
 
@@ -10,6 +11,22 @@ import quoteRoutes from "./routes/quotes.routes.js";
 dotenv.config();
 const app = express();
 // 미들웨어 설정해주기
+// Helmet 설정
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"], // 기본적으로 자기 자신('self')에서만 리소스 로드 허용
+        scriptSrc: ["'self'"], // 외부 JavaScript 로딩 금지
+        styleSrc: ["'self'", "https://fonts.googleapis.com"], // 구글 폰트 허용
+        imgSrc: ["'self'", "data:"], // 데이터 URI 이미지 허용
+        connectSrc: ["'self'", "https://api.example.com"], // API 요청 허용
+        frameSrc: ["'none'"], // iframe 사용 금지
+        objectSrc: ["'none'"], // 플래시 같은 object 태그 사용 금지
+      },
+    },
+  })
+);
 
 app.use(express.json({ limit: "6 mb" }));
 app.use((req, res, next) => {
